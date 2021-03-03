47033d54a3935bb77b02c3ee19880fec8c2dc61d
Nesel <matthieu0608@hotmail.fr>
2021-03-03 23:34:17 +0100

    Création du projet et mise en place de la class contact

---
+++ Projet_Gestion_Contacts/src/main/java/bdd/Entities/Contact.java
@@ -0,0 +1,30 @@
+package bdd.Entities;
+
+import java.security.PrivateKey;
+import java.util.Date;
+
+public class Contact {
+
+    private Integer id;
+    private String name;
+    private String surname;
+    private String nickname;
+    private String phoneFix;
+    private String phoneMobil;
+    private String fax;
+    private Date birthday;
+    private String mail;
+    private String society;
+    private String city;
+    private String postCode;
+    private String country;
+    private String region;
+    private String website;
+    private String streetName;
+    private Integer streetNumber;
+    private String gender;
+    private String streetType;
+
+
+
+}
