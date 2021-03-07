efdb15a42744c3b1c6e75b6373f2a13a239a73a3
Nesel <matthieu0608@hotmail.fr>
2021-03-07 12:50:06 +0100

    Création des test unitaires et des fonctions de la DAO List contact et addcontact

--- Projet_Gestion_Contacts/src/main/java/bdd/Daos/ContactDao.java
+++ Projet_Gestion_Contacts/src/main/java/bdd/Daos/ContactDao.java
@@ -3,10 +3,7 @@ package bdd.Daos;
 import bdd.Entities.Contact;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -26,7 +23,7 @@ public class ContactDao {
                 try(ResultSet resultSet = statement.executeQuery()){ //Recupere le resultat de la requete SQL
                     while(resultSet.next()){
                         LocalDate birth = resultSet.getDate("birth_date").toLocalDate();
-                        int id = resultSet.getInt("genre_id");
+                        int id = resultSet.getInt("id");
                         String firstname = resultSet.getString("firstname");
                         String lastname = resultSet.getString("lastname");
                         String nickname = resultSet.getString("nickname");
@@ -34,7 +31,7 @@ public class ContactDao {
                         String phoneMobil = resultSet.getString("phone_number_mobil");
                         String mail = resultSet.getString("email_address");
                         String address = resultSet.getString("address");
-                        String website = resultSet.getString("website");
+                        String website = resultSet.getString("website_address");
                         Contact contact = new Contact(id, firstname, lastname, nickname, phoneFix, phoneMobil, birth, mail, website, address);
                         list.add(contact);
                     }
@@ -47,6 +44,34 @@ public class ContactDao {
             e.printStackTrace();
         }
         return list;
+    }
+
+    public void addContact(Contact contact) {
+        /*
+         * Function that permit to add a contact to the database
+         */
+        DataSource db = DataSourceFactory.getDataSource();
+        try (Connection connection = db.getConnection()) {
+            String sqlQuery = "INSERT INTO contact(lastname,firstname,nickname,phone_number_fix,phone_number_mobil,address,email_address,website_address,birth_date) " +
+                    "VALUES(?,?,?,?,?,?,?,?,?)";
+            try (PreparedStatement statement = connection.prepareStatement(sqlQuery, Statement.RETURN_GENERATED_KEYS)) {
+                statement.setString(1, contact.getLastname());
+                statement.setString(2, contact.getFirstname());
+                statement.setString(3, contact.getNickname());
+                statement.setString(4, contact.getPhoneFix());
+                statement.setString(5, contact.getPhoneMobil());
+                statement.setString(6, contact.getAdress());
+                statement.setString(7, contact.getMail());
+                statement.setString(8, contact.getWebsite());
+                statement.setDate(9, java.sql.Date.valueOf(contact.getBirthday()));
+
+                statement.executeUpdate();
+                statement.close();
+            }
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 }
---
+++ Projet_Gestion_Contacts/src/test/java/bdd/Daos/ContactDaoTest.java
@@ -0,0 +1,85 @@
+package bdd.Daos;
+
+import bdd.Entities.Contact;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+import static org.junit.Assert.*;
+
+public class ContactDaoTest {
+
+    private ContactDao contactDao = new ContactDao();
+
+    @Before
+    public void initDatabase() throws Exception {
+        Connection connection = DataSourceFactory.getDataSource().getConnection();
+        Statement stmt = connection.createStatement();
+        stmt.executeUpdate(
+                "CREATE TABLE IF NOT EXISTS contact(" +
+                        "    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, \n" +
+                        "    lastname VARCHAR(45) NOT NULL,  \n" +
+                        "    firstname VARCHAR(45) NOT NULL,\n" +
+                        "    nickname VARCHAR(45) NOT NULL,\n" +
+                        "    phone_number_fix VARCHAR(15) NULL,\n" +
+                        "    phone_number_mobil VARCHAR(15) NULL,\n" +
+                        "    address VARCHAR(200) NULL,\n" +
+                        "    email_address VARCHAR(150) NULL,\n" +
+                        "    website_address VARCHAR(200) NULL,\n" +
+                        "    birth_date DATE NULL);");
+        stmt.executeUpdate("DELETE FROM contact");
+        stmt.executeUpdate("INSERT INTO contact(id,lastname,firstname,nickname,phone_number_fix,phone_number_mobil,address,email_address,website_address,birth_date) VALUES (1,'LICETTE', 'Matthieu', 'Math', null,'0602053033','Rue des lilas,59000 LILLE','matthieu.licette@junia.com', null, '1996-08-06 12:00:00.000')");
+        stmt.executeUpdate("INSERT INTO contact(id,lastname,firstname,nickname,phone_number_fix,phone_number_mobil,address,email_address,website_address,birth_date) VALUES (2,'LICETTE', 'Camille', 'caca', null,'0655772137','Rue des lilas,59000 LILLE','camille.licette@hei.yncrea.fr', null, '1998-10-16 12:00:00.000')");
+        stmt.close();
+        connection.close();
+    }
+
+    @Test
+    public void shouldListContacts() {
+        // WHEN
+        List<Contact> contacts = contactDao.listContacts();
+        // THEN
+        assertThat(contacts).hasSize(2);
+        assertThat(contacts).extracting("id", "lastname", "firstname", "nickname", "phoneFix", "phoneMobil", "adress", "mail", "website", "birthday").containsOnly(
+                tuple(1, "LICETTE","Matthieu","Math", null,"0602053033","Rue des lilas,59000 LILLE","matthieu.licette@junia.com", null, LocalDate.of(1996, 8, 6)),
+                tuple(2, "LICETTE","Camille","caca", null,"0655772137","Rue des lilas,59000 LILLE","camille.licette@hei.yncrea.fr", null, LocalDate.of(1998, 10, 16))
+        );
+    }
+
+    @Test
+    public void shouldAddContact() throws Exception {
+        // WHEN
+        Contact contact = new Contact(null, "SNOW", "John", "le batard", "0105112233", null, LocalDate.of(1852, 10, 3), null, null,"Winterfell appartement 240, Westeros");
+        contactDao.addContact(contact);
+
+        // THEN
+        Connection connection = DataSourceFactory.getDataSource().getConnection();
+        Statement statement = connection.createStatement();
+        ResultSet resultSetFilm = statement.executeQuery("SELECT * FROM contact WHERE firstname='SNOW'");
+
+
+        assertThat(resultSetFilm.next()).isTrue();
+        assertThat(resultSetFilm.getInt("id")).isNotNull();
+        assertThat(resultSetFilm.getString("firstname")).isEqualTo("SNOW");
+        assertThat(resultSetFilm.getString("lastname")).isEqualTo("John");
+        assertThat(resultSetFilm.getString("nickname")).isEqualTo("le batard");
+        assertThat(resultSetFilm.getString("phone_number_fix")).isEqualTo("0105112233");
+        assertThat(resultSetFilm.getString("address")).isEqualTo("Winterfell appartement 240, Westeros");
+        assertThat(resultSetFilm.getString("phone_number_mobil")).isNull();
+        assertThat(resultSetFilm.getString("email_address")).isNull();
+        assertThat(resultSetFilm.getString("website_address")).isNull();
+        assertThat(resultSetFilm.getDate("birth_date")).isEqualTo(java.sql.Date.valueOf((LocalDate.of(1852, 10, 3))));
+        assertThat(resultSetFilm.next()).isFalse();
+
+        resultSetFilm.close();
+        statement.close();
+        connection.close();
+    }
+}
