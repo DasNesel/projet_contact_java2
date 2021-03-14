c170d90763974ce8daefee9338c6ed7c44ae7479
Nesel <matthieu0608@hotmail.fr>
2021-03-14 18:03:42 +0100

    Modification vCard ainsi que ses tests

--- Projet_Gestion_Contacts/src/test/java/Controler/vcardTest.java
+++ Projet_Gestion_Contacts/src/test/java/Controler/vcardTest.java
@@ -9,16 +9,16 @@ import java.time.LocalDate;
 import static org.junit.Assert.*;
 
 public class vcardTest {
-/*
+
     @Test
     public void generate() {
-        Contact contact = new Contact(1, "SNOW", "John", "le batard", "0105112233", null, LocalDate.of(1852, 10, 3), null, null,"Winterfell appartement 240, Westeros");
+        Contact contact = new Contact(1, "John", "SNOW", "le batard", "0105112233", null, LocalDate.of(1852, 10, 3), null, null,"Chateau appartement 240", "Winterfell", "Westeros");
         vcard vcardJo = new vcard(contact);
         vcardJo.generate();
         File file = new File("vcard/" + contact.getFirstname() + "_" + contact.getLastname() + ".vcf");
         assertTrue(file.exists());
     }
-*/
+
     @Test
     public void importation() {
 
